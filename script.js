fe4b00f70f399fb35d473694f6ad600cc9d8c5f5
bigmartha1 <tpham112@gmail.com>
2022-02-10 22:26:04 -0800

    feat: add smooth scrolling

--- script.js
+++ script.js
@@ -2,26 +2,40 @@
 let menuButton = document.querySelector(".menu-button");
 let cancelButton = document.querySelector(".cancel-button");
 let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
 
 menuButton.onclick = function() {
     menuButton.style.opacity = "0";
     menuButton.style.pointerEvents = "none";
     navBar.classList.add("active");
+    body.style.overflow = "hidden";
 }
 
 cancelButton.onclick = function() {
     menuButton.style.opacity = "1";
     menuButton.style.pointerEvents = "auto";
     navBar.classList.remove("active");
+    body.style.overflow = "auto";
 }
 
 // Sticky Navigation Menu JS Code //
-let nav = document.querySelector("nav");
 let val;
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 20) {
-        nav.classList.add("sticky");
-    } else {
-        nav.classList.remove("sticky");
+    let nav = document.querySelector("nav");
+    window.onscroll = function() {
+        if (document.documentElement.scrollTop > 20) {
+            nav.classList.add("sticky");
+        } else {
+            nav.classList.remove("sticky");
+        }
     }
+
+// Navigation Close When Links Clicked JS Code //
+let navLinks = document.querySelectorAll(".menu li a");
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", () => {
+        menuButton.style.opacity = "1";
+        menuButton.style.pointerEvents = "auto";
+        navBar.classList.remove("active");
+        body.style.overflow = "auto";
+    })
 }
